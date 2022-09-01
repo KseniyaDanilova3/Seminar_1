5be8f1a1d05b0499cf797d426a6146bf70fa23a1
Kseniya <pashkina.ks@mail.ru>
2022-09-01 23:54:38 +0700

    1задача

---
+++ 001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+
+string input = Console.ReadLine(); // ввод
+
+int number = int.Parse(input); // число
+// double.Parse
+
+// if(int.TryParse (input))
+
+int square = number * number; // квадрат
+
+Console.WriteLine($"Число в квадрате = {square}");
+// Console.WriteLine("Число в квадрате = {0}, square);

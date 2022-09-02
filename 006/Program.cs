5cfe2670faaa69bd8e3c14edd3eeb9be2a16357c
Kseniya <pashkina.ks@mail.ru>
2022-09-02 19:28:54 +0700

    6 задача

---
+++ 006/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.WriteLine ("Введите некоторое число");
+
+string userIn = Console.ReadLine ();
+
+int number = int.Parse (userIn);
+
+int count = number;
+
+number = -1 * number;
+
+while (number <= count) {
+
+    Console.Write ($"{number} ");
+    number += 1;
+} 

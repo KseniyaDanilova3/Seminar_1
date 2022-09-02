b95a20fff8a88aab1eff7bb07a9aa6b162929d8f
Kseniya <pashkina.ks@mail.ru>
2022-09-02 19:21:14 +0700

    2 задача

---
+++ 002/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+Console.WriteLine("Введите число 1");
+
+string userIn_1 = Console.ReadLine();
+
+int number_1 = int.Parse(userIn_1);
+
+Console.WriteLine("Введите число 2, которое мы будем сравнивать с первым, возведенным в квадрат");
+
+string userIn_2 = Console.ReadLine();
+
+int number_2 = int.Parse(userIn_2);
+
+int sqr = number_1 * number_1;
+
+if (sqr == number_2) {
+
+    Console.WriteLine($"Квадрат числа {number_1} действительно равен {number_2}");
+}
+
+else {
+
+    Console.WriteLine($"Квадрат числа {number_1} не равен {number_2}");
+}

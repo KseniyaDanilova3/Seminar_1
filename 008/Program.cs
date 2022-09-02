a5dcbe5fd0fe19762615147a930128c27e0fbc56
Kseniya <pashkina.ks@mail.ru>
2022-09-02 19:31:39 +0700

    8 задача

---
+++ 008/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.WriteLine ("Введите некоторое число от 0 до 32767 :)");
+
+string str = Console.ReadLine ();
+
+int number = int.Parse (str);
+
+Console.WriteLine(number % 10);
